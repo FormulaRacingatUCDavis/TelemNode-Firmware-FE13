7ff9cb503362dc3dbd503352168e297205c5414a
Vi-L <viclei1216@gmail.com>
2026-05-02 11:03:36 -0700

    add heartbeat

--- TelemNode-Firmware-FE13/Core/Src/main.c
+++ TelemNode-Firmware-FE13/Core/Src/main.c
@@ -125,6 +125,8 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  HAL_GPIO_TogglePin(HEARTBEAT_GPIO_Port, HEARTBEAT_Pin);
+	  HAL_Delay(5);
   }
   /* USER CODE END 3 */
 }
